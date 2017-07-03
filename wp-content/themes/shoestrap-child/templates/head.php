519b47217d47c245200af7472299922d00eb728b
hfreemanlaw <hfreemanlaw@a2plcpnl0201.prod.iad2.secureserver.net>
2017-07-02 17:07:38 -0700

    Updates

---
+++ wp-content/themes/shoestrap-child/templates/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <?php wp_head(); ?>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+    <link rel='stylesheet' href='/wp-content/themes/shoestrap-child/style.css' type='text/css' media='all' />
+    <?php if ( !is_user_logged_in() ){ ?><style>#wpadminbar{ display:none; }html {margin-top:0 !important;}</style><?php } ?>
+</head>
